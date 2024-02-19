f1c89fb128be32712f2903c34e3eaba4beada7b7
James (personal) <jh5723@ic.ac.uk>
2024-02-19 18:19:10 +0000

    Feat: Added complete CRUD functionality for user accounts

---
+++ api/services/eventManager.py
@@ -0,0 +1,39 @@
+####################################################################################################
+# Project Name: Motive Event Management System
+# File: eventManager.py
+# Description:
+#
+# Authors: James Hartley, Ankur Desai, Patrick Borman, Julius Gasson, and Vadim Dunaevskiy
+# Date: 2024-02-19
+# Version: 1.0
+#
+# Notes:
+####################################################################################################
+
+
+# # def create_event(request):
+#
+# # def update_event(request):
+#
+# # def delete_event(request):
+#
+# # def fetch_event_info(request):
+#     """Fetches information on a specific event"""
+#     """Should include ability to fetch outstanding tickets"""
+#     # object_type = request.get('object_type')
+#
+# # def fetch_users_events(request):
+#     """Fetches IDs and information for all of a user's events. Depending on the user type, a
+#     different helper function is called."""
+#
+# # def fetch_venues_events(request):
+#     """Fetches IDs and information for each event being hosted by a specific venue"""
+#     """Includes the total number of tickets and the number remaining unsold"""
+#
+# # def fetch_artists_events(request):
+#     """Fetches IDs and information for each event an artist is performing at"""
+#
+# # def fetch_users_upcoming_events():
+#     """Fetches IDss and information for all future events a user has a ticket for in future"""
+#
+# # def fetch_events_near_postcode(postcode, max_distance)
---
+++ api/services/ticketManager.py
@@ -0,0 +1,26 @@
+####################################################################################################
+# Project Name: Motive Event Management System
+# File: ticketManager.py
+# Description:
+#
+# Authors: James Hartley, Ankur Desai, Patrick Borman, Julius Gasson, and Vadim Dunaevskiy
+# Date: 2024-02-19
+# Version: 1.0
+#
+# Notes:
+####################################################################################################
+
+
+# # def create_ticket(request):
+#
+# # def update_ticket(request):
+#
+# # def delete_ticket(request):
+#
+# # def fetch_ticket_info():
+#     """Fetches information for a specific ticket"""
+#
+# # def fetch_users_tickets():
+#     """Fetches IDs and information for all tickets owned by a particular user"""
+
+
