43726643ccc251b022a6556e643ea40f6a2dda7f
Julius Gasson <jg2023@ic.ac.uk>
2024-02-22 18:48:50 +0000

    ci: added stub tests to testing directories (to be replaced)

--- api/tests/unit/utils/test_nothing.py
+++ api/tests/unit/utils/test_nothing.py
@@ -2,7 +2,7 @@
 # Project Name: Motive Event Management System
 # Course: COMP70025 - Software Systems Engineering
 # File: test_nothing.py
-# Description: This file contains a stub test to ensure that the integration testing framework is working.
+# Description: This file contains a stub test to ensure that the utils unit testing framework is working.
 #
 # Authors: James Hartley, Ankur Desai, Patrick Borman, Julius Gasson, and Vadim Dunaevskiy
 # Date: 2024-02-22
