b057f0adfe9a68847db6514b1d4b6a6064902552
Julius Gasson <jg2023@ic.ac.uk>
2024-02-22 21:51:17 +0000

    ci: Check to see how an unapproved change affects staging and production environments

--- api/app.py
+++ api/app.py
@@ -112,7 +112,8 @@ def login_required(f):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Change made in staging environment"
+    # return render_template("index.html")
 
 
 @app.route("/register")
