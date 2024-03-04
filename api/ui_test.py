c25f5b7e1803ce0684046be20f6b734d120552af
avdes96 <140278723+avdes96@users.noreply.github.com>
2024-03-04 18:54:37 +0000

    Test: Check clicking logo goes back to homepage

--- api/ui_test.py
+++ api/ui_test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
 import sys
 
 # Path to your WebDriver executable (change the path as needed)
@@ -9,16 +10,17 @@ import sys
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
+homepage = "https://jumpstartevents.co.uk"
+
 try:
     # Navigate to the web application
-    driver.get("https://jumpstartevents.co.uk")
+    driver.get(homepage)
     # Optionally, wait for the page to load or for specific elements to become available
     # This example waits up to 10 seconds for the page title to be available
     driver.implicitly_wait(10)
 
     # Check the title of the page
     assert "Home" in driver.title
-    assert "Login" in driver.title
 
     # Perform additional tests, such as finding elements or interacting with the page
     # For example, to find an element by its ID and enter text:
@@ -32,6 +34,14 @@ except AssertionError:
 except WebDriverException:
     print("Test failed: WebDriver error occurred. Is Flask running?")
     sys.exit(1)
+
+try:
+    logo = driver.find_element(By.ID, "logo")
+    logo.click()
+    driver.implicitly_wait(10)  
+    assert driver.current_url == homepage
+
+    print("Successfully clicked on the logo and navigated to the homepage.")
 finally:
-    # Close the browser window
+    # Clean up: close the browser window
     driver.quit()
